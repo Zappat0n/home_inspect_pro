f11e7ac619290a6aecb835395cb7978cca954088
Angel Luis Barros Pazos <albarros1973@gmail.com>
2026-06-05 19:39:03 +0200

    Add devise

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module HomeInspectPro
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # I18n configuration
+    config.i18n.available_locales = %i[en es]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
