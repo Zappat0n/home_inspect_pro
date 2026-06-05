ac751978ec765dfe63235b8aaa03c4b5c84e659d
Angel Luis Barros Pazos <albarros1973@gmail.com>
2026-06-05 21:32:01 +0200

    Add annotaterb gem

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  id         :bigint           not null, primary key
+#  available  :boolean
+#  code       :string
+#  is_default :boolean
+#  locale     :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Country < ApplicationRecord
   has_many :users
 end
