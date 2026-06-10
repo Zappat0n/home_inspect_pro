e386306ac638941033754104a4a49183d6d5462d
Angel Luis Barros Pazos <albarros1973@gmail.com>
2026-06-10 20:01:24 +0200

    billing safeguards

--- app/jobs/generate_pdf_report_job.rb
+++ app/jobs/generate_pdf_report_job.rb
@@ -7,6 +7,11 @@ class GeneratePdfReportJob < ApplicationJob
 
   def perform(inspection_id, base_url)
     Inspection.find(inspection_id).then do |inspection|
+      unless inspection.user.subscribed? || inspection.user.on_trial?
+        Rails.logger.warn("Skipping PDF generation for inspection #{inspection_id}: user not subscribed or on trial")
+        return nil
+      end
+
       Rails.logger.info("Generating PDF report for inspection #{inspection_id}")
       PdfReportService.new(inspection, base_url: base_url).call
       Rails.logger.info("PDF report generated for inspection #{inspection_id}")
